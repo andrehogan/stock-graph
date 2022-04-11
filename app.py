e6990759065d2294dc399d64671611b9ad3cd2ca
André Hogan <w0lf@w0lf.local>
2022-04-10 18:14:05 -1000

    second commit

--- app.py
+++ app.py
@@ -2,6 +2,7 @@ import pandas as pd
 import yfinance as yf
 from datetime import datetime
 from datetime import timedelta
+from plotly.subplots import make_subplots
 import plotly.graph_objects as go
 from fbprophet import Prophet
 from fbprophet.plot import plot_plotly, plot_components_plotly
@@ -9,46 +10,18 @@ import warnings
 
 warnings.filterwarnings('ignore')
 
-def graphstock():
-    # plot the open price
-    x = df["ds"]
-    y = df["y"]
-
-    fig = go.Figure()
-
-    fig.add_trace(go.Scatter(x=x, y=y))
-
-    # Set title
-    fig.update_layout(
-        title_text='Time series plot of %s Open Price' % (stock),
-    )
-
-    fig.update_layout(
-        xaxis=dict(
-            rangeselector=dict(
-                buttons=list(
-                    [
-                        dict(count=1, label="1m", step="month", stepmode="backward"),
-                        dict(count=6, label="6m", step="month", stepmode="backward"),
-                        dict(count=1, label="YTD", step="year", stepmode="todate"),
-                        dict(count=1, label="1y", step="year", stepmode="backward"),
-                        dict(step="all"),
-                    ]
-                )
-            ),
-            rangeslider=dict(visible=True),
-            type="date",
-        )
-    )
-    fig.show()
-
 pd.options.display.float_format = '${:,.2f}'.format
 
 today = datetime.today().strftime('%Y-%m-%d')
 start_date = '2016-01-01'
 
 stockname = ["AMD", "ETH-USD", "SHIB-USD"]
-for stock in stockname:
+
+fig = make_subplots(
+    rows=len(stockname), cols=1,
+    subplot_titles=(stockname))
+
+for i, stock in enumerate(stockname):
     print(stock)
     stock_df = yf.download(stock,start_date, today)
 
@@ -73,4 +46,50 @@ for stock in stockname:
     df.rename(columns=new_names, inplace=True)
 
     df.tail()
-    graphstock()
+
+    # plot the open price
+    x = df["ds"]
+    y = df["y"]
+
+    fig.append_trace(go.Scatter(
+        x = x, 
+        y = y,
+    ),  row = i + 1, col = 1)
+
+    fig.update_xaxes(
+        rangeselector=dict(
+            buttons=list(
+                [
+                    dict(count=1, label="1m", step="month", stepmode="backward"),
+                    dict(count=6, label="6m", step="month", stepmode="backward"),
+                    dict(count=1, label="YTD", step="year", stepmode="todate"),
+                    dict(count=1, label="1y", step="year", stepmode="backward"),
+                    dict(step="all"),
+                ]
+            )
+        ),
+    )
+
+fig.show()
+
+def ml_forecast():
+    m = Prophet(
+        seasonality_mode="multiplicative",
+        yearly_seasonality=True,
+        daily_seasonality=True
+    )
+
+    m.fit(df)
+
+    future = m.make_future_dataframe(periods = 365)
+    future.tail()
+
+    forecast = m.predict(future)
+    forecast[['ds', 'yhat', 'yhat_lower', 'yhat_upper']].tail()
+
+    next_day = (datetime.today() + timedelta(days=1)).strftime('%Y-%m-%d')
+
+    forecast[forecast['ds'] == next_day]['yhat'].item()
+
+    plot_plotly(m, forecast)
+    plot_components_plotly(m, forecast)
